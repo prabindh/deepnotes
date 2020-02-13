ae60c6bc946108fd09cb7d55df0f870c8f61368f
Prabindh Sundareson <prabindh@yahoo.com>
2020-02-13 16:09:10 +0530

    updated test for cpu and gpu

--- rapids/cudf-test.py
+++ rapids/cudf-test.py
@@ -6,64 +6,54 @@ import cudf as pd_cudf
 
 results_groupby = []
 
-chunk_size = 1000000
 profile_count = 5
 ### Read in the data with Pandas
 
 cpu_pd = True
 gpu_pd = True
 
-if cpu_pd:
-	print ("Running CPU, size = ", chunk_size)
-	for run in range(0, profile_count):
-		dfList = []
+def get_chunked_df_size(csv_name, delimiter, encoding):
+	chunk_size = 1000000
+	for run in range(0, 100):
+		df_list = []
 		count = 0
 		size = 0
 		try:
-			chunkreader = pd.read_csv("DM_ALUNO.CSV",
-					delimiter="|",
-					encoding="latin-1", low_memory=False, chunksize=chunk_size)
+			chunkreader = pd.read_csv(csv_name,
+					delimiter=delimiter,
+					encoding=encoding, low_memory=False, chunksize=chunk_size)
+			for df in chunkreader:
+				count = count + 1
+				df_list.append(df)
+				size = size + count * chunk_size
+				break # one big chunk is sufficient for this benchmarking
+			break
 		except:
-			print ("Could not load chunk size: ", chunk_size)
 			chunk_size = chunk_size / 100
 			continue
-		for df in chunkreader:
-			count = count + 1
-			dfList.append(df)
-			size = size + count * chunk_size
-			print ("size: ", size)
-			break
-		df = pd.concat(dfList,sort=False)
-		    
+	print ("Selected chunk size: ", chunk_size)
+
+	return df_list, chunk_size
+
+df_list, chunk_size = get_chunked_df_size("DM_ALUNO.CSV", "|", "latin-1")
+
+if cpu_pd:
+	print ("Running CPU, size = ", chunk_size)
+	df = pd.concat(df_list,sort=False)
+	for run in range(0, profile_count):
+    
 		s = time.time()
 		df = df.groupby("CO_IES").size()
 		e = time.time()
 		results_groupby.append({"lib":"Pandas","time":float("{}".format(e-s))})
 		print("Pandas Groupby Time = {}".format(e-s))
 
-
 if gpu_pd:
 	print ("Running GPU, size = ", chunk_size)
+	df = pd_cudf.concat(df_list,sort=False)
 	### Read in the data with cudf
 	for run in range(0, profile_count):
 		dfList = []
-		count = 0
-		size = 0
-		try:
-			chunkreader = pd.read_csv("DM_ALUNO.CSV",
-					delimiter="|",
-					encoding="latin-1", low_memory=False, chunksize=chunk_size)
-		except:
-			print ("Could not load chunk size: ", chunk_size)
-			chunk_size = chunk_size / 100
-			continue
-		for df in chunkreader:
-			count = count + 1
-			dfList.append(df)
-			size = size + count * chunk_size
-			print ("size: ", size)
-			break
-		df = pd_cudf.concat(dfList,sort=False)
 	    
 		s = time.time()
 		df = df.groupby("CO_IES").size()
